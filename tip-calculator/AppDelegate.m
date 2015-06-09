c119333dcc5d6e112e58b1e3ae58f283ef282e1c
salagadoola <salagadoola@ymail.com>
2015-06-09 14:31:58 +0800

    Set rootViewController to TipViewController

--- tip-calculator/AppDelegate.m
+++ tip-calculator/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TipViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    TipViewController *vc = [[TipViewController alloc] init];
+    self.window.rootViewController = vc;
+    
     return YES;
 }
 
